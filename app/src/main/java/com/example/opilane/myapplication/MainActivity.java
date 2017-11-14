ecc6ac79e936a1e572ee917659f0123ae3f0d804
Opilane TTHK <opilane@tthk.ee>
2017-11-14 11:44:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opilane/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.opilane.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    EditText tekst;
+    Button send;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tekst= (EditText)findViewById(R.id.test)  ;
+        send=(Button)findViewById(R.id.btnSend) ;
+
+
+
+}
+
+    public void saada(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.test);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
